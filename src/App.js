9bf8c3a8dba1cf0308bbe00222730b564865f62d
Inga Safranova <93312251+IngaSafranova@users.noreply.github.com>
2022-05-16 21:39:57 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,58 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import "./App.css";
+import React, { useState } from "react";
+import buttonImage from "./Button.png";
+import Energisers from "../Energisers";
+import Quotes from "../Quotes";
+import Reminders from "../Reminders";
+import Button from "../Button";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
+import Title from "../Title";
+
+function App() {
+  const [id, setId] = useState(1);
+
+  function handleClick() {
+    const number = Math.ceil(Math.random() * 10);
+    if (number !== id) {
+      setId(number);
+    }
   }
 
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+  return (
+    <main>
+      {/* <button onClick={handleClick}>pls click me</button> */}
+      {/* <div>
+        <Title> </Title>
+        <div id="button1">
+          {" "}
+          <button onClick={handleClick}>
+            <img src={buttonImage} alt="title" />
+          </button>
+        </div>
+        <Energisers text="Energise!" id={id}></Energisers>
+        <Quotes text="Your daily good vibes!" id={id}></Quotes>
+        <Reminders text="Remember!" id={id}></Reminders>
+      </div> */}
+      <div class="topcontainer">
+        <div class="leftcolumn">
+          <Reminders text="Remember!" id={id}></Reminders>
+        </div>
+        <div class="centrecolumn">
+          <Title> </Title>
+          <div id="button1">
+            {" "}
+            {/* <button onClick={handleClick}>
+              <img src={buttonImage} alt="title" />
+            </button> */}
+            <Button handleClick={handleClick} buttonImage={buttonImage} text="Zen"/>
+          </div>
+          <Energisers text="Energise!" id={id}></Energisers>
+        </div>
+        <div class="rightcolumn">
+          <Quotes text="Your daily good vibes!" id={id}></Quotes>
+        </div>
       </div>
-    )
-  }
+    </main>
+  );
 }
-
-export default App
+export default App;
